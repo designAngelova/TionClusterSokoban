7fae9be0bc031b7bd64d20d87c5151f80f68ee5f
g-stoyanov <georgi.j.stoyanov@gmail.com>
2014-05-15 10:27:18 +0300

    test

--- src/gameMain/SokobanGame.java
+++ src/gameMain/SokobanGame.java
@@ -3,7 +3,7 @@ package gameMain;
 public class SokobanGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 System.out.println(" | 1  2  3  4  5  6  7  8  9");
 
 	}
 
