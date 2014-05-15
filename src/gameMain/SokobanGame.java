132c9f293db3110eb07879b4558a54ef526a78da
designAngelova <design.angelova@gmail.com>
2014-05-15 10:51:19 +0300

    test

--- src/gameMain/SokobanGame.java
+++ src/gameMain/SokobanGame.java
@@ -4,10 +4,9 @@ public class SokobanGame {
 
 	public static void main(String[] args) {
 		 System.out.println(" | 1  2  3  4  5  6  7  8  9");
-<<<<<<< HEAD
+
 		 System.out.println("Test comit");
-=======
->>>>>>> refs/remotes/origin/master
+
 
 	}
 
