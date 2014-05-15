0e9347c610de46bce5b6954a8c31d5ab7d20da9f
designAngelova <design.angelova@gmail.com>
2014-05-15 10:41:24 +0300

    test

--- src/gameMain/SokobanGame.java
+++ src/gameMain/SokobanGame.java
@@ -3,7 +3,8 @@ package gameMain;
 public class SokobanGame {
 
 	public static void main(String[] args) {
-		System.out.println("Dryn Dryn");
+		 System.out.println(" | 1  2  3  4  5  6  7  8  9");
+		 System.out.println("Test comit");
 
 	}
 
