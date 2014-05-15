bb2584e3f518bea8429edc1ce3813d94856fc2ca
g-stoyanov <georgi.j.stoyanov@gmail.com>
2014-05-15 13:54:08 +0300

    Added class GameField and Game

---
+++ src/gameClasses/Game.java
@@ -0,0 +1,13 @@
+package gameClasses;
+
+public class Game {
+	private String statusLine;
+	private String drawer;
+	private GameField field;
+	private String gameMenu;
+	private String nextMenu;
+	private String levelMenu;
+	private String scores;
+	private GameController controller;
+
+}
