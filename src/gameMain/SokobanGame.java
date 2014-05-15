e99a2efc93a810f482ac8c4eaef89eb442434ae2
designAngelova <design.angelova@gmail.com>
2014-05-15 11:05:12 +0300

    test

--- src/gameMain/SokobanGame.java
+++ src/gameMain/SokobanGame.java
@@ -6,6 +6,7 @@ public class SokobanGame {
 		 System.out.println(" | 1  2  3  4  5  6  7  8  9");
 		 System.out.println(" | 1  2  3  4  5  6  7  8  9");
 		 System.out.println(" | 1  2  3  4  5  6  7  8  9");
+		 System.out.println(" Test Comit");
 	}
 
 }
