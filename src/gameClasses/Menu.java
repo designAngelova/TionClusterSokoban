0b43d5f174f997dec0f35c5ea5de2ea4f83c1c33
g-stoyanov <georgi.j.stoyanov@gmail.com>
2014-05-16 17:27:00 +0300

    Fix bugs in class Menu

--- src/gameClasses/Menu.java
+++ src/gameClasses/Menu.java
@@ -1,77 +1,70 @@
 package gameClasses;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Menu{
-	
+public class Menu {
+
 	public Menu() {
-		@SuppressWarnings("unused")
-		List<MenuItem> items = new ArrayList<MenuItem>();;
-	}
-	
-	List<MenuItem> items;
-	
-	@SuppressWarnings("unused")
-	private List<MenuItem> getItems() {
-		  return items;
+		this.items = new ArrayList<MenuItem>();
 	}
 
-	@SuppressWarnings("unused")
-	private void setItems(List<MenuItem> items) {
-		  this.items = items;
+	List<MenuItem> items;
+
+	public List<MenuItem> getItems() {
+		return this.items;
 	}
-	
-	@SuppressWarnings("unused")
-	private void addItem(MenuItem item) {
-		  this.items.add( item);
-		}
-	
+
+	public void setItems(List<MenuItem> items) {
+		this.items = items;
+	}
+
+	public void addItem(MenuItem item) {
+		this.items.add(item);
+	}
+
 	public void selectNext() {
-		for (int i = 0; i < items.size() - 1; i++) {
-			
-			if (items.get(i).getId() == items.size() - 1) {
-				items.get(i).setIsSelected(false);
-				items.get(0).setIsSelected(true);
+		for (int i = 0; i < this.items.size() - 1; i++) {
+			if (this.items.get(i).getIsSelected()) {
+				if (this.items.get(i).getId() == items.size() - 1) {
+					this.items.get(i).setIsSelected(false);
+					this.items.get(0).setIsSelected(true);
+				} else {
+					this.items.get(i).setIsSelected(false);
+					this.items.get(i + 1).setIsSelected(true);
+					break;
 				}
-			
-			
-			
-			if (items.get(i).getIsSelected()) {
-				items.get(i).setIsSelected(false);
-				items.get(i+1).setIsSelected(true);
+
+				break;
 			}
 		}
-	
 	}
-	
+
 	public void selectPrev() {
 		for (int i = 0; i < items.size() - 1; i++) {
-			
-			if (items.get(i).getId() == 0) {
-				items.get(i).setIsSelected(false);
-				items.get(items.size() - 1).setIsSelected(true);
+			if (this.items.get(i).getIsSelected()) {
+				if (items.get(i).getId() == 0) {
+					items.get(i).setIsSelected(false);
+					items.get(items.size() - 1).setIsSelected(true);
+				} else {
+					items.get(i).setIsSelected(false);
+					items.get(i - 1).setIsSelected(true);
 				}
-			
-			
-			
-			if (items.get(i).getIsSelected()) {
-				items.get(i).setIsSelected(false);
-				items.get(i-1).setIsSelected(true);
+				
+				break;
 			}
-
 		}
-	
 	}
-	
-	@SuppressWarnings("unused")
-	private int getSelectedId() {
+
+	public int getSelectedId() {
 		int a = 0;
 		for (int i = 0; i < items.size() - 1; i++) {
 			if (items.get(i).getIsSelected()) {
 				a = items.get(i).getId();
+				break;
 			}
 		}
+
 		return a;
 	}
-	
-	}
+}
