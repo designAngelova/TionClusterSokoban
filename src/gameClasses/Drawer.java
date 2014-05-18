9ee0d6dbdb083fe325bf08128d7bc4ede808d97d
Emi2014 <emilia.kolarova@gmail.com>
2014-05-18 22:53:56 +0300

    drawer test

---
+++ src/gameClasses/Drawer.java
@@ -0,0 +1,14 @@
+package gameClasses;
+
+
+
+
+public abstract class Drawer {
+
+	 abstract void drawMenu(Menu menu) ;
+	 abstract void drawScores(Scores scores);
+	 abstract void drawGameField(GameField field);
+	 abstract void drawStatusLine(StatusLine status);
+}
+
+
